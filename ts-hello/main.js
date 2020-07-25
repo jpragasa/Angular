abb8ddcaa5690b12739a740c7a6779100e2e6529
jpragasa <jpragasa@seu.edu>
2020-07-25 16:24:03 -0400

    Angular course in progress

---
+++ ts-hello/main.js
@@ -0,0 +1,15 @@
+// function log(message)
+// {
+//     console.log(message);
+// }
+// var message = 'Hello World';
+// log(message)
+// var number = 1;
+// let count = 2;
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log('Finally: ' + i);
+}
+doSomething();
---
+++ ts-hello/main.ts
@@ -0,0 +1,20 @@
+// function log(message)
+// {
+//     console.log(message);
+// }
+
+// var message = 'Hello World';
+
+// log(message)
+
+// var number = 1;
+// let count = 2;
+
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log('Finally: ' + i);
+}
+
+doSomething();
