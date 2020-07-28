6c32f09a6c000bb3834952caf10fef38bf15eef8
jpragasa <jpragasa@seu.edu>
2020-07-28 11:59:16 -0400

    Added updated content from tutorial

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -13,3 +13,61 @@ function doSomething() {
     console.log('Finally: ' + i);
 }
 doSomething();
+// TypeScript Types
+var a;
+var b;
+var c;
+var d;
+var e = [1, 2, 3];
+var f = [1, true, 'a', false];
+var ColorRed = 0;
+var ColorGreen = 1;
+var ColorBlue = 2;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 0] = "Green";
+    Color[Color["Blue"] = 0] = "Blue";
+    Color[Color["Purple"] = 3] = "Purple";
+})(Color || (Color = {}));
+;
+var backgroundColor = Color.Blue;
+var backgroundColor2 = Color.Red;
+var backgroundColor3 = Color.Green;
+// Types of Assertions
+var message;
+message = 'abc';
+var endsWithC = message.endsWith('c');
+var alternative = message.endsWith('c');
+// Arrow Functions
+var log = function (message) {
+    console.log(message);
+};
+var doLog = function (message) { return console.log(message); };
+var doLogWithNoParams = function () { return console.log(message); };
+//Interfaces Allow you to predefine an object, cannot have implementation, only a signature
+// interface Point {
+//     x: number,
+//     y: number,
+//     draw: () => void
+// }
+// This works but is verbose:
+//  let drawPoint = (point: {x: number, y: number}) => {//code//}
+// This is why we would use interfaces like the above
+// Cohesion: Things that need to be together should stay together
+// Class: groups variables (properties) and functions (methods) that are highly related
+var Point = /** @class */ (function () {
+    function Point() {
+    }
+    Point.prototype.draw = function () {
+        console.log('x: ' + this.x + ', y: ' + this.y);
+    };
+    Point.prototype.getDistance = function (another) {
+    };
+    return Point;
+}());
+//Object use for the above class
+var point = new Point();
+point.x = 3;
+point.y = 5;
+point.draw();
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -18,3 +18,73 @@ function doSomething() {
 }
 
 doSomething();
+
+// TypeScript Types
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1, 2, 3];
+let f: any[] = [1, true, 'a', false];
+
+const ColorRed = 0;
+const ColorGreen = 1;
+const ColorBlue = 2;
+
+enum Color {Red = 0, Green = 0, Blue = 0, Purple = 3};
+let backgroundColor = Color.Blue;
+let backgroundColor2 = Color.Red;
+let backgroundColor3 = Color.Green;
+
+// Types of Assertions
+let message;
+message = 'abc';
+let endsWithC = (<string>message).endsWith('c');
+let alternative = (message as string).endsWith('c');
+
+// Arrow Functions
+let log = function(message)
+{
+    console.log(message);
+}
+
+let doLog = (message) => console.log(message);
+let doLogWithNoParams = () => console.log(message);
+
+    //Interfaces Allow you to predefine an object, cannot have implementation, only a signature
+    // interface Point {
+    //     x: number,
+    //     y: number,
+    //     draw: () => void
+    // }
+
+    // This works but is verbose:
+    //  let drawPoint = (point: {x: number, y: number}) => {//code//}
+    // This is why we would use interfaces like the above
+
+
+    // Cohesion: Things that need to be together should stay together
+
+    // Class: groups variables (properties) and functions (methods) that are highly related
+
+class Point {
+    x: number;
+    y: number;
+    draw() {
+        console.log('x: ' + this.x + ', y: ' + this.y);
+    }
+
+    getDistance(another: Point) {
+
+    }
+}
+
+//Object use for the above class
+let point = new Point();
+point.x = 3
+point.y = 5
+point.draw();
+
+
+
+
